c1501d38ce764925e196efdbbe78dc0fb4a02b85
Li Rundian <lirundian2010@gmail.com>
2025-05-06 18:34:08 +0900

    catagories-update

--- php/laravel/app/Http/Controllers/Controller.php
+++ php/laravel/app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public ?int $perPage = null;
+
+    public function __construct()
+    {
+        $this->perPage = config('app.per_page', 5);
+    }
 }
